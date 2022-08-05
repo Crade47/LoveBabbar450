21dd7f5fb75307e0f4d59bcdf43d6f595fa62c69
Devansh <devanshsoni79@gmail.com>
2022-08-05 15:53:10 +0530

    Started tripletSum question.

---
+++ Array/tripletSum.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool find3Numbers(int A[], int n, int X);
+int main(){
+    	int size,key;
+		cin>>size>>key;
+		int i,A[size];
+		for(i=0;i<n;i++)
+			cin>>A[i];
+		cout << find3Numbers(A, n, X) << endl;
+return 0;
+}
+
+bool find3Numbers(int A[], int n, int X){
+
+    
+
+}
